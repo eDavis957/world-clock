c6ef54dad86729cdc7c1e653c56715289e8527c9
eDavis957 <stefficodes@gmail.com>
2024-11-20 17:55:31 -0800

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -45,7 +45,7 @@ function updateCity(event) {
           )} <small>${cityTime.format("A")}</small>
         </div>
   </div>
-  <a href="index.html">All cities</a>
+  <a href="/">All cities</a>
         `;
 }
 
